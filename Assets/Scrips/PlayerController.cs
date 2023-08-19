c07c816e1e02b4c215f2bfa9269c7addc000fb13
PabloCS <titanbenevolo@icloud.com>
2023-08-18 23:18:44 -0300

    Setup incial.

---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float currentJumpForce = 2f;
+    [SerializeField] float climbSpeed = 2f;
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float maxJumpForce = 10f;
+    [SerializeField] float jumpChargeRate = 2f;
+
+    public int maxJumps = 1;
+
+    private int remainingJumps;
+    private bool isOnWall = false;
+    private Vector2 wallNormal;
+    private bool isClimbing = false;
+
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        remainingJumps = maxJumps;
+    }
+
+    void Start()
+    {
+        IncreaseJumps(1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (remainingJumps > 0 || (isOnWall && rb.velocity.y <= 0)))
+        {
+            currentJumpForce = 5f;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentJumpForce = Mathf.Clamp(currentJumpForce + Time.deltaTime * jumpChargeRate, 0f, maxJumpForce);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (isOnWall && Input.GetKey(KeyCode.Space))
+        {
+            isClimbing = true;
+        }
+        else
+        {
+            isClimbing = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput != 0 && !isClimbing)
+        {
+            Move(horizontalInput);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x * 0.9f, rb.velocity.y);
+        }
+
+        if (isClimbing)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, climbSpeed);
+        }
+    }
+
+    private void Jump()
+    {
+        if (currentJumpForce > 0)
+        {
+            if (isOnWall)
+            {
+                rb.velocity = new Vector2(-wallNormal.x * moveSpeed, currentJumpForce);
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, currentJumpForce);
+                remainingJumps--;
+            }
+
+            currentJumpForce = 2f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            remainingJumps = maxJumps;
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            isOnWall = true;
+            wallNormal = collision.contacts[0].normal.normalized;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            // Para que no se tranque en las esquinas?
+            isOnWall = true;
+            wallNormal = collision.contacts[0].normal.normalized;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            isOnWall = false;
+        }
+    }
+
+    private void Move(float direction)
+    {
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+    }
+
+    public void IncreaseJumps(int amount)
+    {
+        maxJumps += amount;
+        remainingJumps += amount;
+    }
+}
