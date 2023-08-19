077a607558d6ed691ad33e1d183dbd416635c7b8
PabloCS <titanbenevolo@icloud.com>
2023-08-19 15:55:02 -0300

    Ya no puede saltar sobre el muro.

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -49,7 +49,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space) && !isClimbing)
         {
             Jump();
         }
