5b406f5bb3c4ed40b90cf39ee6649b3014fb1067
Alvin <alvaro.ainfante@gmail.com>
2023-08-20 16:09:39 -0300

    sound manager

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource backgroundMusic;
+    [SerializeField] AudioSource walk;
+    [SerializeField] AudioSource preparejump;
+    [SerializeField] AudioSource jump;
+    [SerializeField] AudioSource climb;
+    [SerializeField] AudioSource land;
+
+    public static SoundManager Instance { get; private set; }
+
+    
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    public AudioSource GetBGM() => backgroundMusic;
+    public AudioSource GetWalk() => walk;
+    public AudioSource GetPrepareJump() => preparejump;
+    public AudioSource GetJump() => jump;
+    public AudioSource GetClimb() => climb;
+    public AudioSource GetLand() => land;
+    
+    
+    
+}
