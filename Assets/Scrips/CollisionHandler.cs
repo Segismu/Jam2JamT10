59a1ccbe0aa07129e7d9231b1b4073f10ce4861c
PabloCS <titanbenevolo@icloud.com>
2023-08-20 12:16:09 -0300

    Player Tag y animaciones en prefab parte 1.

---
+++ Assets/Scrips/CollisionHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("StartAnimation", true);
+        }
+    }
+}
