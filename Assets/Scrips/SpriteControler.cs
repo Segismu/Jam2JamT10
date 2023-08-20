e663d5d3dc79e3baeb2822786d7d11c4c26c3512
Alvin <alvaro.ainfante@gmail.com>
2023-08-20 10:26:00 -0300

    fixed timming animations

--- Assets/Scrips/SpriteControler.cs
+++ Assets/Scrips/SpriteControler.cs
@@ -8,10 +8,6 @@ public class SpriteControler : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] Animator animator;
-    [SerializeField] Sprite idle;
-    [SerializeField] Sprite prepareJump;
-    [SerializeField] Sprite jump;
-    [SerializeField] Sprite climb;
 
     void Update()
     {
@@ -20,23 +16,19 @@ public class SpriteControler : MonoBehaviour
             case CatState.Jump:
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("jump");
-                spriteRenderer.sprite = jump;
                 break;
             case CatState.PreparingJump:
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("prepare_jump");
-                spriteRenderer.sprite = prepareJump;
                 break;
             case CatState.ClimbRightWall:
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("climb");
-                spriteRenderer.sprite = climb;
                 spriteRenderer.flipX = false;
                 break;
             case CatState.ClimbLeftWall:
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("climb");
-                spriteRenderer.sprite = climb;
                 spriteRenderer.flipX = true;
                 break;
             case CatState.Walk:
@@ -53,7 +45,6 @@ public class SpriteControler : MonoBehaviour
         {
             animator.SetBool("cat_walk", false);
             animator.SetTrigger("idle");
-            spriteRenderer.sprite = idle;
         }else if (horizontalInput > 0)
         {
             animator.SetBool("cat_walk", true);
