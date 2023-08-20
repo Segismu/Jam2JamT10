b0915d24bc36cd71e34300aeaef67929e393ffa7
Alvin <alvaro.ainfante@gmail.com>
2023-08-20 10:36:42 -0300

    change animation when jumping from wall

--- Assets/Scrips/SpriteControler.cs
+++ Assets/Scrips/SpriteControler.cs
@@ -8,32 +8,48 @@ public class SpriteControler : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] Animator animator;
+    bool alreadyFlippedFromWallJump = false;
 
     void Update()
     {
         switch (PlayerController.State)
         {
             case CatState.Jump:
+                alreadyFlippedFromWallJump = false;
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("jump");
                 break;
             case CatState.PreparingJump:
+                alreadyFlippedFromWallJump = false;
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("prepare_jump");
                 break;
             case CatState.ClimbRightWall:
+                alreadyFlippedFromWallJump = false;
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("climb");
                 spriteRenderer.flipX = false;
                 break;
             case CatState.ClimbLeftWall:
+                alreadyFlippedFromWallJump = false;
                 animator.SetBool("cat_walk", false);
                 animator.SetTrigger("climb");
                 spriteRenderer.flipX = true;
                 break;
             case CatState.Walk:
+                alreadyFlippedFromWallJump = false;
                 DefineWalkSprite();
                 break;
+            case CatState.WallJump:
+                animator.SetBool("cat_walk", false);
+                animator.SetTrigger("jump");
+                if (!alreadyFlippedFromWallJump)
+                {
+                    alreadyFlippedFromWallJump = true;
+                    spriteRenderer.flipX = !spriteRenderer.flipX;
+                }
+                break;
+                
         }
 
     }
